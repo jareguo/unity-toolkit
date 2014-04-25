db58a619380e7546421845f8d6eb4b262054b93e
Jare Guo <jare85@163.com>
2014-04-25 15:13:15 +0800

    small fix and refactoring

--- ex2D/FSM/AnimState.cs
+++ ex2D/FSM/AnimState.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using fsm;
+
+namespace fsm {
 
 /// <summary>
 /// 动画状态机的跳转，实例由AnimState.to方法进行创建。
@@ -8,7 +9,7 @@ using fsm;
 /// 如果要多个条件之间进行或(or)运算，可以创建多个AnimTransition实例。
 /// </summary>
 [System.Serializable]
-public class AniTransition : TimerTransition {
+public class AnimTransition : TimerTransition {
 
     /// <summary> 触发动画跳转，不论when、after条件是否满足。 </summary>
     public bool trigger = false;
@@ -20,7 +21,7 @@ public class AniTransition : TimerTransition {
     // Functions
     ///////////////////////////////////////////////////////////////////////////////
     
-    public AniTransition ()
+    public AnimTransition ()
         : base () {
         onCheck = OnCheck;
     }
@@ -30,7 +31,7 @@ public class AniTransition : TimerTransition {
     /// 条件函数，返回是否可以跳转。
     /// 这个方法不能有任何副作用，如果需要修改变量，应该在state的回调里修改！
     /// </param>
-    public AniTransition when (System.Func<bool> _onCheck = null) {
+    public AnimTransition when (System.Func<bool> _onCheck = null) {
         onDoCheck = _onCheck;
         return this;
     }
@@ -41,7 +42,7 @@ public class AniTransition : TimerTransition {
     /// 如果是0.5则代表必须等到动画播放到一半后才能切换到其它状态。
     /// 使用normalizedTime进行判断，例如是2.5则代表要等动画loop 2.5次后才能切换到其它状态。
     /// </param>
-    public AniTransition after (float _normalizedTime) {
+    public AnimTransition after (float _normalizedTime) {
         exitTime = _normalizedTime;
         return this;
     }
@@ -52,15 +53,15 @@ public class AniTransition : TimerTransition {
             return true;
         }
 
-        float normalizedTime = ((AniState)source).normalizedTime;
+        float normalizedTime = ((AnimState)source).normalizedTime;
         bool played = normalizedTime == 0.0f;
         if (played) {
             normalizedTime = 1.0f;
 #if UNITY_EDITOR
             if (exitTime.HasValue != false && exitTime.Value > 1.0f) {
-                var s = (AniState)source;
-                var state = s.anim[s.curAniName];
-                if (state != null && (state.wrapMode != WrapMode.Loop || state.wrapMode != WrapMode.PingPong || state.wrapMode != WrapMode.ClampForever)) {
+                var s = (AnimState)source;
+                var state = s.anim[s.curAnimName];
+                if (state != null && (state.wrapMode != WrapMode.Loop && state.wrapMode != WrapMode.PingPong && state.wrapMode != WrapMode.ClampForever)) {
                     Debug.LogWarning(string.Format("非循环动画的normalizedTime永远不可能大于1 ({0} to {1})", source.name, target.name));
 	            }
             }
@@ -81,10 +82,10 @@ public class AniTransition : TimerTransition {
 /// 动画状态机的基本状态，跳转到新状态时将播放对应动作
 /// </summary>
 [System.Serializable]
-public class AniState : State {
+public class AnimState : State {
 
     public Animation anim = null;
-    public string curAniName = null;
+    public string curAnimName = null;
 
     ///////////////////////////////////////////////////////////////////////////////
     // Properties
@@ -93,9 +94,9 @@ public class AniState : State {
     public float normalizedTime {
         get {
             //AnimationState animState = anim.GetPlayingAnimation();
-            AnimationState aniState = anim[curAniName];
+            AnimationState animState = anim[curAnimName];
             //Debug.Log(string.Format("animState: {0} " + animState.normalizedTime, animState.name));
-            return aniState != null ? aniState.normalizedTime : 0.0f;
+            return animState != null ? animState.normalizedTime : 0.0f;
         }
     }
 
@@ -104,7 +105,7 @@ public class AniState : State {
     ///////////////////////////////////////////////////////////////////////////////
     
     /// <param name="_name"> 状态名，用于调试，同时作为当前状态所播放的动作名(也就是AnimationClip.name) </param>
-    public AniState (string _name, Animation _anim, State _parent = null) 
+    public AnimState (string _name, Animation _anim, State _parent = null) 
         : base(_name, _parent) {
         anim = _anim;
         mgDebug.Assert(anim != null);
@@ -118,8 +119,8 @@ public class AniState : State {
     /// <summary> 声明状态的跳转 </summary>
     /// <param name="_targetState"> 目标状态 </param>
     /// <param name="_duration"> 动画过渡所需时间 </param>
-    public AniTransition to (AniState _targetState, float _duration) {
-        AniTransition transition = new AniTransition() {
+    public AnimTransition to (AnimState _targetState, float _duration) {
+        AnimTransition transition = new AnimTransition() {
             source = this,
             target = _targetState,
             duration = _duration,
@@ -136,7 +137,7 @@ public class AniState : State {
     protected void DoPlay (Transition transition, string animName) {
         if (string.IsNullOrEmpty(animName)) {
             anim.Stop();
-            curAniName = null;
+            curAnimName = null;
             return;
         }
         if (transition == null ||
@@ -146,26 +147,70 @@ public class AniState : State {
         else {
             anim.CrossFade(animName, ((TimerTransition)transition).duration);
         }
-        curAniName = animName;
+        curAnimName = animName;
     }
 }
 
+namespace Detail {
+
+    /// <summary>
+    /// 包含多个动作的状态
+    /// </summary>
+    [System.Serializable]
+    public abstract class MultiAnimStateBase : AnimState {
+        public string[] animList = null;
+
+        /// <param name="_animList"> 以";"分割的动画名称列表 </param>
+        /// <param name="_name"> 状态名，用于调试 </param>
+        public MultiAnimStateBase (string _name, Animation _anim, string _animList, State _parent = null)
+            : base(_name, _anim, _parent) {
+            animList = _animList.Split(new char[] { ';' }, System.StringSplitOptions.RemoveEmptyEntries);
+            if (animList.Length == 0) {
+                Debug.LogError("no animation is specified");
+            }
+        }
+    }
+
+}
+
 /// <summary>
-/// 随机动作状态，用于播放一系列随机动作
+/// 自动切换动作的状态，用于播放不同动作
 /// </summary>
 [System.Serializable]
-public class RandAniState : AniState {
-    public string[] animList = null;
+public class MultiAnimState : Detail.MultiAnimStateBase {
+    public enum SelectType {
+        Sequence,   // 依次循环播放一系列动作
+        Random,     // 随机播放
+    }
+
+    SelectType selectType;
+    int lastAnimIndex = -1;
 
     /// <param name="_animList"> 以";"分割的动画名称列表 </param>
     /// <param name="_name"> 状态名，用于调试 </param>
-    public RandAniState (string _name, Animation _anim, string _animList, State _parent = null)
-        : base(_name, _anim, _parent) {
-        animList = _animList.Split(new char[] { ';' }, System.StringSplitOptions.RemoveEmptyEntries);
+    public MultiAnimState (string _name, Animation _anim, string _animList, SelectType _selectType, State _parent = null)
+        : base(_name, _anim, _animList, _parent) {
+        selectType = _selectType;
     }
-    
     public override void Play (Transition transition) {
-        var animName = animList[Random.Range(0, animList.Length)];
-        DoPlay(transition, animName);
+        switch (selectType) {
+        case SelectType.Sequence:
+            lastAnimIndex++;
+            lastAnimIndex %= animList.Length;
+            DoPlay(transition, animList[lastAnimIndex]);
+            break;
+        case SelectType.Random:
+            DoPlay(transition, animList[Random.Range(0, animList.Length)]);
+            break;
+        }
     }
+    /// <summary> 重置播放索引 </summary>
+    public void Reset() {
+        exDebug.Assert(selectType == SelectType.Sequence);
+        lastAnimIndex = -1;
+    }
+}
+
+//public class BlendTree : Detail.MultiAnimStateBase
+
 }
